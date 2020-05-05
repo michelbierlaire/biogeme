d81d8303463a2d6bf51610b4b472337896f8b9cb
michelbierlaire <michel.bierlaire@epfl.ch>
2020-05-05 17:57:44 +0200

    Update 05normalMixtureMonteCarlo.py

--- examples/montecarlo/05normalMixtureMonteCarlo.py
+++ examples/montecarlo/05normalMixtureMonteCarlo.py
@@ -9,23 +9,25 @@ with various types of draws.
 
 """
 
+# pylint: disable=invalid-name, undefined-variable
+
 import pandas as pd
 import biogeme.database as db
 import biogeme.biogeme as bio
-import biogeme.draws as draws
-import biogeme.distributions as dist
 import biogeme.models as models
+import biogeme.distributions as dist
+from biogeme.expressions import Integrate, RandomVariable, MonteCarlo, bioDraws
 
-from biogeme.expressions import Beta, DefineVariable, RandomVariable, Integrate, MonteCarlo, bioDraws 
-
-p = pd.read_csv("swissmetro.dat",sep='\t')
-# Use only the first observation
+p = pd.read_csv('swissmetro.dat', sep='\t')
+# Use only the first observation (index 0)
 p = p.drop(p[p.index != 0].index)
-database = db.Database("swissmetro",p)
+database = db.Database('swissmetro', p)
 
+# The following statement allows you to use the names of the variable
+# as Python variable.
 globals().update(database.variables)
 
-#Parameters 
+#Parameters
 ASC_CAR = 0.137
 ASC_TRAIN = -0.402
 ASC_SM = 0
@@ -33,48 +35,31 @@ B_TIME = -2.26
 B_TIME_S = 1.66
 B_COST = -1.29
 
-# Define a random parameter, normally distributed, 
-# designed to be used for integration
 omega = RandomVariable('omega')
-density = dist.normalpdf(omega) 
+density = dist.normalpdf(omega)
 B_TIME_RND = B_TIME + B_TIME_S * omega
-B_TIME_RND_normal = B_TIME + B_TIME_S * \
-                    bioDraws('B_NORMAL','NORMAL')
-B_TIME_RND_anti = B_TIME + B_TIME_S * \
-                  bioDraws('B_ANTI','NORMAL_ANTI')
-B_TIME_RND_halton = B_TIME + B_TIME_S * \
-                    bioDraws('B_HALTON','NORMAL_HALTON2')
-B_TIME_RND_mlhs = B_TIME + B_TIME_S * bioDraws('B_MLHS','NORMAL_MLHS')
-B_TIME_RND_antimlhs = B_TIME + B_TIME_S * \
-                      bioDraws('B_ANTIMLHS','NORMAL_MLHS_ANTI')
+B_TIME_RND_normal = B_TIME + B_TIME_S * bioDraws('B_NORMAL', 'NORMAL')
+B_TIME_RND_anti = B_TIME + B_TIME_S * bioDraws('B_ANTI', 'NORMAL_ANTI')
+B_TIME_RND_halton = B_TIME + B_TIME_S * bioDraws('B_HALTON', 'NORMAL_HALTON2')
+B_TIME_RND_mlhs = B_TIME + B_TIME_S * bioDraws('B_MLHS', 'NORMAL_MLHS')
+B_TIME_RND_antimlhs = B_TIME + B_TIME_S * bioDraws('B_ANTIMLHS', 'NORMAL_MLHS_ANTI')
 
-# Utility functions
-
-#If the person has a GA (season ticket) her 
-#incremental cost is actually 0 
-#rather than the cost value gathered from the
-# network data. 
-SM_COST =  SM_CO   * (  GA   ==  0  ) 
-TRAIN_COST =  TRAIN_CO   * (  GA   ==  0  )
-
-# For numerical reasons, it is good practice to scale the data to
-# that the values of the parameters are around 1.0. 
-# A previous estimation with the unscaled data has generated
-# parameters around -0.01 for both cost and time. 
-# Therefore, time and cost are multipled my 0.01.
-
-TRAIN_TT_SCALED = \
-  DefineVariable('TRAIN_TT_SCALED', TRAIN_TT / 100.0,database)
-TRAIN_COST_SCALED = \
-  DefineVariable('TRAIN_COST_SCALED', TRAIN_COST / 100,database)
-SM_TT_SCALED = DefineVariable('SM_TT_SCALED', SM_TT / 100.0,database)
-SM_COST_SCALED = DefineVariable('SM_COST_SCALED', SM_COST / 100,database)
-CAR_TT_SCALED = DefineVariable('CAR_TT_SCALED', CAR_TT / 100,database)
-CAR_CO_SCALED = DefineVariable('CAR_CO_SCALED', CAR_CO / 100,database)
-CAR_AV_SP =  DefineVariable('CAR_AV_SP',CAR_AV  * (  SP   !=  0  ),database)
-TRAIN_AV_SP =  DefineVariable('TRAIN_AV_SP',TRAIN_AV  * (  SP   !=  0  ),database)
+# Definition of new variables
+SM_COST = SM_CO * (GA == 0)
+TRAIN_COST = TRAIN_CO * (GA == 0)
+CAR_AV_SP = CAR_AV * (SP != 0)
+TRAIN_AV_SP = TRAIN_AV * (SP != 0)
+TRAIN_TT_SCALED = TRAIN_TT / 100.0
+TRAIN_COST_SCALED = TRAIN_COST / 100
+SM_TT_SCALED = SM_TT / 100.0
+SM_COST_SCALED = SM_COST / 100
+CAR_TT_SCALED = CAR_TT / 100
+CAR_CO_SCALED = CAR_CO / 100
 
 def logit(THE_B_TIME_RND):
+    """
+    Calculate the conditional logit model for a given random parameter.
+    """
     V1 = ASC_TRAIN + \
          THE_B_TIME_RND * TRAIN_TT_SCALED + \
          B_COST * TRAIN_COST_SCALED
@@ -96,10 +81,10 @@ def logit(THE_B_TIME_RND):
           3: CAR_AV_SP}
 
     # The choice model is a logit, with availability conditions
-    integrand = models.logit(V,av,CHOICE)
+    integrand = models.logit(V, av, CHOICE)
     return integrand
 
-numericalI = Integrate(logit(B_TIME_RND)*density,'omega')
+numericalI = Integrate(logit(B_TIME_RND) * density, 'omega')
 normal = MonteCarlo(logit(B_TIME_RND_normal))
 anti = MonteCarlo(logit(B_TIME_RND_anti))
 halton = MonteCarlo(logit(B_TIME_RND_halton))
@@ -114,8 +99,8 @@ simulate = {'Numerical': numericalI,
             'Antithetic MLHS': antimlhs}
 
 R = 20000
-biogeme = bio.BIOGEME(database,simulate,numberOfDraws=R)
+biogeme = bio.BIOGEME(database, simulate, numberOfDraws=R)
 results = biogeme.simulate()
-print(f'Number of draws: {10*R}')
+print(f'Number of draws: {R}')
 for c in results.columns:
     print(f'{c}:\t{results.loc[0,c]}')
