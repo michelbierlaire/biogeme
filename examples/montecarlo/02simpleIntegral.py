e8b819fdf18b60e1e169f859d91bfb5832a8a671
michelbierlaire <michel.bierlaire@epfl.ch>
2020-05-05 17:57:35 +0200

    Update 02simpleIntegral.py

--- examples/montecarlo/02simpleIntegral.py
+++ examples/montecarlo/02simpleIntegral.py
@@ -10,6 +10,8 @@ available in Biogeme.
 
 """
 
+# pylint: disable=invalid-name, undefined-variable
+
 import pandas as pd
 import biogeme.database as db
 import biogeme.biogeme as bio
@@ -21,58 +23,53 @@ from biogeme.expressions import exp, bioDraws, MonteCarlo
 # to store the draws
 pandas = pd.DataFrame()
 pandas['FakeColumn'] = [1.0]
-database = db.Database('fakeDatabase',pandas)
+database = db.Database('fakeDatabase', pandas)
 
-# The user can define new draws. For example, Halton draws
-# with base 13, skipping the first 10 draws.
-
-def halton13(sampleSize,numberOfDraws):
+def halton13(sampleSize, numberOfDraws):
+    """
+    The user can define new draws. For example, Halton draws
+    with base 13, skipping the first 10 draws.
+    """
     return draws.getHaltonDraws(sampleSize,
                                 numberOfDraws,
                                 base=13,
                                 skip=10)
 
-mydraws = {'HALTON13':(halton13,'Halton draws, base 13, skipping 10')}
+mydraws = {'HALTON13': (halton13, 'Halton draws, base 13, skipping 10')}
 database.setRandomNumberGenerators(mydraws)
 
-
-integrand = exp(bioDraws('U','UNIFORM'))
+integrand = exp(bioDraws('U', 'UNIFORM'))
 simulatedI = MonteCarlo(integrand)
 
-integrand_halton = exp(bioDraws('U_halton','UNIFORM_HALTON2'))
+integrand_halton = exp(bioDraws('U_halton', 'UNIFORM_HALTON2'))
 simulatedI_halton = MonteCarlo(integrand_halton)
 
-integrand_halton13 = exp(bioDraws('U_halton13','HALTON13'))
+integrand_halton13 = exp(bioDraws('U_halton13', 'HALTON13'))
 simulatedI_halton13 = MonteCarlo(integrand_halton13)
 
-integrand_mlhs = exp(bioDraws('U_mlhs','UNIFORM_MLHS'))
+integrand_mlhs = exp(bioDraws('U_mlhs', 'UNIFORM_MLHS'))
 simulatedI_mlhs = MonteCarlo(integrand_mlhs)
 
-trueI = exp(1.0) - 1.0 
+trueI = exp(1.0) - 1.0
 
 R = 20000
 
-sampleVariance = \
-  MonteCarlo(integrand*integrand) - simulatedI * simulatedI
+sampleVariance = MonteCarlo(integrand * integrand) - simulatedI * simulatedI
 stderr = (sampleVariance / R)**0.5
 error = simulatedI - trueI
 
-sampleVariance_halton = \
-  MonteCarlo(integrand_halton*integrand_halton) \
-  - simulatedI_halton * simulatedI_halton
+sampleVariance_halton = MonteCarlo(integrand_halton * integrand_halton) - \
+    simulatedI_halton * simulatedI_halton
 stderr_halton = (sampleVariance_halton / R)**0.5
 error_halton = simulatedI_halton - trueI
 
-sampleVariance_halton13 = \
-  MonteCarlo(integrand_halton13*integrand_halton13) \
-  - simulatedI_halton13 * simulatedI_halton13
+sampleVariance_halton13 = MonteCarlo(integrand_halton13 * integrand_halton13) - \
+    simulatedI_halton13 * simulatedI_halton13
 stderr_halton13 = (sampleVariance_halton13 / R)**0.5
 error_halton13 = simulatedI_halton13 - trueI
 
-
-sampleVariance_mlhs = \
-  MonteCarlo(integrand_mlhs*integrand_mlhs) \
-  - simulatedI_mlhs * simulatedI_mlhs
+sampleVariance_mlhs = MonteCarlo(integrand_mlhs * integrand_mlhs) - \
+    simulatedI_mlhs * simulatedI_mlhs
 stderr_mlhs = (sampleVariance_mlhs / R)**0.5
 error_mlhs = simulatedI_mlhs - trueI
 
@@ -95,15 +92,24 @@ simulate = {'Analytical Integral': trueI,
             'Std Error (MLHS)         ': stderr_mlhs,
             'Error (MLHS)             ': error_mlhs}
 
-
-
-biogeme = bio.BIOGEME(database,simulate,numberOfDraws=R)
+biogeme = bio.BIOGEME(database, simulate, numberOfDraws=R)
 biogeme.modelName = '02simpleIntegral'
 results = biogeme.simulate()
-print(f"Analytical integral:{results.iloc[0]['Analytical Integral']:.6g}")
-print(f"\t\tUniform\t\tHalton\t\tHalton13\tMLHS")
-print(f"Simulated\t{results.iloc[0]['Simulated Integral']:.6g}\t{results.iloc[0]['Simulated Integral (Halton)']:.6g}\t{results.iloc[0]['Simulated Integral (Halton13)']:.6g}\t{results.iloc[0]['Simulated Integral (MLHS)']:.6g}")
-print(f"Sample var.\t{results.iloc[0]['Sample variance   ']:.6g}\t{results.iloc[0]['Sample variance (Halton)   ']:.6g}\t{results.iloc[0]['Sample variance (Halton13)   ']:.6g}\t{results.iloc[0]['Sample variance (MLHS)   ']:.6g}")
-print(f"Std error\t{results.iloc[0]['Std Error         ']:.6g}\t{results.iloc[0]['Std Error (Halton)         ']:.6g}\t{results.iloc[0]['Std Error (Halton13)         ']:.6g}\t{results.iloc[0]['Std Error (MLHS)         ']:.6g}")
-print(f"Error\t\t{results.iloc[0]['Error             ']:.6g}\t{results.iloc[0]['Error (Halton)             ']:.6g}\t{results.iloc[0]['Error (Halton13)             ']:.6g}\t{results.iloc[0]['Error (MLHS)             ']:.6g}")
-
+print(f'Analytical integral:{results.iloc[0]["Analytical Integral"]:.6g}')
+print(f'\t\tUniform\t\tHalton\t\tHalton13\tMLHS')
+print(f'Simulated\t{results.iloc[0]["Simulated Integral"]:.6g}\t'
+      f'{results.iloc[0]["Simulated Integral (Halton)"]:.6g}\t'
+      f'{results.iloc[0]["Simulated Integral (Halton13)"]:.6g}\t'
+      f'{results.iloc[0]["Simulated Integral (MLHS)"]:.6g}')
+print(f'Sample var.\t{results.iloc[0]["Sample variance   "]:.6g}\t'
+      f'{results.iloc[0]["Sample variance (Halton)   "]:.6g}\t'
+      f'{results.iloc[0]["Sample variance (Halton13)   "]:.6g}\t'
+      f'{results.iloc[0]["Sample variance (MLHS)   "]:.6g}')
+print(f'Std error\t{results.iloc[0]["Std Error         "]:.6g}\t'
+      f'{results.iloc[0]["Std Error (Halton)         "]:.6g}\t'
+      f'{results.iloc[0]["Std Error (Halton13)         "]:.6g}\t'
+      f'{results.iloc[0]["Std Error (MLHS)         "]:.6g}')
+print(f'Error\t\t{results.iloc[0]["Error             "]:.6g}\t'
+      f'{results.iloc[0]["Error (Halton)             "]:.6g}\t'
+      f'{results.iloc[0]["Error (Halton13)             "]:.6g}\t'
+      f'{results.iloc[0]["Error (MLHS)             "]:.6g}')
