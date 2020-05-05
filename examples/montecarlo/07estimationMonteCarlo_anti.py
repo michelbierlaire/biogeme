c0387c5a487c630fe750ee6a140a535f832ebd36
michelbierlaire <michel.bierlaire@epfl.ch>
2020-05-05 17:57:53 +0200

    Update 07estimationMonteCarlo_anti.py

--- examples/montecarlo/07estimationMonteCarlo_anti.py
+++ examples/montecarlo/07estimationMonteCarlo_anti.py
@@ -10,67 +10,58 @@ approximated using MonteCarlo integration, with antithetic draws.
 
 """
 
+# pylint: disable=invalid-name, undefined-variable
+
 import pandas as pd
 import biogeme.database as db
 import biogeme.biogeme as bio
 import biogeme.models as models
-from biogeme.expressions import Beta, DefineVariable, bioDraws, MonteCarlo, log
+from biogeme.expressions import Beta, bioDraws, MonteCarlo, log
 
-pandas = pd.read_csv("swissmetro.dat",sep='\t')
-database = db.Database("swissmetro",pandas)
+pandas = pd.read_csv('swissmetro.dat', sep='\t')
+database = db.Database('swissmetro', pandas)
 
-# The Pandas data structure is available as database.data. Use all the
-# Pandas functions to invesigate the database
-#print(database.data.describe())
-
+# The following statement allows you to use the names of the variable
+# as Python variable.
 globals().update(database.variables)
 
-# Removing some observations can be done directly using pandas.
-#remove = (((database.data.PURPOSE != 1) & (database.data.PURPOSE != 3)) | (database.data.CHOICE == 0))
-#database.data.drop(database.data[remove].index,inplace=True)
-
-# Here we use the "biogeme" way for backward compatibility
-exclude = (( PURPOSE != 1 ) * (  PURPOSE   !=  3  ) +  ( CHOICE == 0 )) > 0
+# Removing some observations
+exclude = ((PURPOSE != 1) * (PURPOSE != 3) + (CHOICE == 0)) > 0
 database.remove(exclude)
 
-
-ASC_CAR = Beta('ASC_CAR',0,None,None,0)
-ASC_TRAIN = Beta('ASC_TRAIN',0,None,None,0)
-ASC_SM = Beta('ASC_SM',0,None,None,1)
-B_TIME = Beta('B_TIME',0,None,None,0)
-B_TIME_S = Beta('B_TIME_S',1,None,None,0)
-B_COST = Beta('B_COST',0,None,None,0)
+ASC_CAR = Beta('ASC_CAR', 0, None, None, 0)
+ASC_TRAIN = Beta('ASC_TRAIN', 0, None, None, 0)
+ASC_SM = Beta('ASC_SM', 0, None, None, 1)
+B_TIME = Beta('B_TIME', 0, None, None, 0)
+B_TIME_S = Beta('B_TIME_S', 1, None, None, 0)
+B_COST = Beta('B_COST', 0, None, None, 0)
 
 # Define a random parameter, normally distributed, designed to be used
 # for Monte-Carlo simulation
-B_TIME_RND = B_TIME + B_TIME_S * bioDraws('B_TIME_RND','NORMAL_ANTI')
+B_TIME_RND = B_TIME + B_TIME_S * bioDraws('B_TIME_RND', 'NORMAL_ANTI')
 
-# Utility functions
+# Definition of new variables
+SM_COST = SM_CO * (GA == 0)
+TRAIN_COST = TRAIN_CO * (GA == 0)
+CAR_AV_SP = CAR_AV * (SP != 0)
+TRAIN_AV_SP = TRAIN_AV * (SP != 0)
+TRAIN_TT_SCALED = TRAIN_TT / 100.0
+TRAIN_COST_SCALED = TRAIN_COST / 100
+SM_TT_SCALED = SM_TT / 100.0
+SM_COST_SCALED = SM_COST / 100
+CAR_TT_SCALED = CAR_TT / 100
+CAR_CO_SCALED = CAR_CO / 100
 
-#If the person has a GA (season ticket) her incremental cost is actually 0 
-#rather than the cost value gathered from the
-# network data. 
-SM_COST =  SM_CO   * (  GA   ==  0  ) 
-TRAIN_COST =  TRAIN_CO   * (  GA   ==  0  )
-
-# For numerical reasons, it is good practice to scale the data to
-# that the values of the parameters are around 1.0. 
-# A previous estimation with the unscaled data has generated
-# parameters around -0.01 for both cost and time. Therefore, time and
-# cost are multipled my 0.01.
-
-TRAIN_TT_SCALED = DefineVariable('TRAIN_TT_SCALED',\
-                                 TRAIN_TT / 100.0,database)
-TRAIN_COST_SCALED = DefineVariable('TRAIN_COST_SCALED',\
-                                   TRAIN_COST / 100,database)
-SM_TT_SCALED = DefineVariable('SM_TT_SCALED', SM_TT / 100.0,database)
-SM_COST_SCALED = DefineVariable('SM_COST_SCALED', SM_COST / 100,database)
-CAR_TT_SCALED = DefineVariable('CAR_TT_SCALED', CAR_TT / 100,database)
-CAR_CO_SCALED = DefineVariable('CAR_CO_SCALED', CAR_CO / 100,database)
-
-V1 = ASC_TRAIN + B_TIME_RND * TRAIN_TT_SCALED + B_COST * TRAIN_COST_SCALED
-V2 = ASC_SM + B_TIME_RND * SM_TT_SCALED + B_COST * SM_COST_SCALED
-V3 = ASC_CAR + B_TIME_RND * CAR_TT_SCALED + B_COST * CAR_CO_SCALED
+# Definition of the utility functions
+V1 = ASC_TRAIN + \
+     B_TIME_RND * TRAIN_TT_SCALED + \
+     B_COST * TRAIN_COST_SCALED
+V2 = ASC_SM + \
+     B_TIME_RND * SM_TT_SCALED + \
+     B_COST * SM_COST_SCALED
+V3 = ASC_CAR + \
+     B_TIME_RND * CAR_TT_SCALED + \
+     B_COST * CAR_CO_SCALED
 
 # Associate utility functions with the numbering of alternatives
 V = {1: V1,
@@ -78,21 +69,20 @@ V = {1: V1,
      3: V3}
 
 # Associate the availability conditions with the alternatives
-
-CAR_AV_SP =  DefineVariable('CAR_AV_SP',CAR_AV  * (  SP   !=  0  ),database)
-TRAIN_AV_SP =  DefineVariable('TRAIN_AV_SP',TRAIN_AV  * (  SP   !=  0  ),database)
-
 av = {1: TRAIN_AV_SP,
       2: SM_AV,
       3: CAR_AV_SP}
 
 # The choice model is a logit, with availability conditions
-prob = models.logit(V,av,CHOICE)
+prob = models.logit(V, av, CHOICE)
 logprob = log(MonteCarlo(prob))
 
-R= 2000
-biogeme = bio.BIOGEME(database,logprob,numberOfDraws=R)
+R = 2000
+biogeme = bio.BIOGEME(database, logprob, numberOfDraws=R)
 
 biogeme.modelName = '07estimationMonteCarlo_anti'
 results = biogeme.estimate()
-results.writeLaTeX()
+
+# Get the results in a pandas table
+pandasResults = results.getEstimatedParameters()
+print(pandasResults)
