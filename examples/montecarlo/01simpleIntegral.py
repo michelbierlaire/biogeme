40375790161557c2e863c72c8d196ac0696e99f5
michelbierlaire <michel.bierlaire@epfl.ch>
2020-05-05 17:57:33 +0200

    Update 01simpleIntegral.py

--- examples/montecarlo/01simpleIntegral.py
+++ examples/montecarlo/01simpleIntegral.py
@@ -6,27 +6,27 @@ Calculation of a simple integral using Monte-Carlo integration.
 
 """
 
+# pylint: disable=invalid-name, undefined-variable
+
 import pandas as pd
 import biogeme.database as db
 import biogeme.biogeme as bio
-import biogeme.draws as draws
 from biogeme.expressions import exp, bioDraws, MonteCarlo
 
 # We create a fake database with one entry, as it is required
 # to store the draws
 pandas = pd.DataFrame()
 pandas['FakeColumn'] = [1.0]
-database = db.Database('fakeDatabase',pandas)
+database = db.Database('fakeDatabase', pandas)
 
-integrand = exp(bioDraws('U','UNIFORM'))
+integrand = exp(bioDraws('U', 'UNIFORM'))
 simulatedI = MonteCarlo(integrand)
 
-trueI = exp(1.0) - 1.0 
+trueI = exp(1.0) - 1.0
 
 R = 2000
 
-sampleVariance = \
-  MonteCarlo(integrand*integrand) - simulatedI * simulatedI
+sampleVariance = MonteCarlo(integrand*integrand) - simulatedI * simulatedI
 stderr = (sampleVariance / R)**0.5
 error = simulatedI - trueI
 
@@ -36,7 +36,7 @@ simulate = {'Analytical Integral': trueI,
             'Std Error         ': stderr,
             'Error             ': error}
 
-biogeme = bio.BIOGEME(database,simulate,numberOfDraws=R)
+biogeme = bio.BIOGEME(database, simulate, numberOfDraws=R)
 biogeme.modelName = f'01simpleIntegral_{R}'
 results = biogeme.simulate()
 print(f'Number of draws: {R}')
@@ -44,12 +44,9 @@ for c in results.columns:
     print(f'{c}: {results.loc[0,c]}')
 
 # With 10 times more draws
-biogeme2 = bio.BIOGEME(database,simulate,numberOfDraws=10*R)
+biogeme2 = bio.BIOGEME(database, simulate, numberOfDraws=10 * R)
 biogeme2.modelName = '01simpleIntegral_{10*R}'
 results2 = biogeme2.simulate()
-print(f'Number of draws: {10*R}')
+print(f'Number of draws: {10 * R}')
 for c in results.columns:
-    print(f'{c}: {results2.loc[0,c]}')
-
-
-
+    print(f'{c}: {results2.loc[0, c]}')
