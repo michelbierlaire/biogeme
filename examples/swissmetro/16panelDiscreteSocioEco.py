8ce852add86c67592689f26efc222f8d3ce1bfe5
michelbierlaire <michel.bierlaire@epfl.ch>
2020-06-03 08:49:24 +0200

    Update 16panelDiscreteSocioEco.py

--- examples/swissmetro/16panelDiscreteSocioEco.py
+++ examples/swissmetro/16panelDiscreteSocioEco.py
@@ -135,7 +135,11 @@ logger.setGeneral()
 biogeme = bio.BIOGEME(database, logprob, numberOfDraws=100000)
 biogeme.modelName = '16panelDiscreteSocioEco'
 
+# As the estimation may take a while and risk to be interrupted, we save the iterations,
+# and restore them before the estimation.
+fname = "__16panelDiscreteSocioEco.iters"
+biogeme.loadSavedIteration(filename=fname)
 # Estimate the parameters.
-results = biogeme.estimate()
+results = biogeme.estimate(saveIterations=True, file_iterations=fname)
 pandasResults = results.getEstimatedParameters()
 print(pandasResults)
