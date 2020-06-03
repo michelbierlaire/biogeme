7e7dc74d453c16334bb765be185b4f5bf12f323a
michelbierlaire <michel.bierlaire@epfl.ch>
2020-06-03 07:22:36 +0200

    Latest estimation results

---
+++ webpage/website.old/backtotop.js
@@ -0,0 +1,16 @@
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
