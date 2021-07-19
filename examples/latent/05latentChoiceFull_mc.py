fbebfb50c619f22814e87174ca7a36600fa6bf37
michelbierlaire <michel.bierlaire@epfl.ch>
2021-07-19 18:34:43 +0200

    Update 05latentChoiceFull_mc.py

--- examples/latent/05latentChoiceFull_mc.py
+++ examples/latent/05latentChoiceFull_mc.py
@@ -13,12 +13,19 @@ import sys
 import pandas as pd
 import biogeme.database as db
 import biogeme.biogeme as bio
-import biogeme.models as models
-import biogeme.distributions as dist
+from biogeme import models
 import biogeme.results as res
 import biogeme.messaging as msg
-from biogeme.expressions import Beta, DefineVariable, log, \
-    bioDraws, MonteCarlo, Elem, bioNormalCdf, exp
+from biogeme.expressions import (
+    Beta,
+    DefineVariable,
+    log,
+    bioDraws,
+    MonteCarlo,
+    Elem,
+    bioNormalCdf,
+    exp,
+)
 
 # Read the data
 df = pd.read_csv('optima.dat', sep='\t')
@@ -35,23 +42,31 @@ database.remove(Choice == -1.0)
 try:
     structResults = res.bioResults(pickleFile='02oneLatentOrdered.pickle')
 except FileNotFoundError:
-    print('Run first the script 02oneLatentOrdered.py in order to generate the file '
-          '02oneLatentOrdered.pickle.')
+    print(
+        'Run first the script 02oneLatentOrdered.py in order to generate the '
+        'file 02oneLatentOrdered.pickle.'
+    )
     sys.exit()
 structBetas = structResults.getBetaValues()
 
 ### Variables
 
 # Piecewise linear definition of income
-ScaledIncome = DefineVariable('ScaledIncome', CalculatedIncome / 1000, database)
+ScaledIncome = DefineVariable(
+    'ScaledIncome', CalculatedIncome / 1000, database
+)
 thresholds = [None, 4, 6, 8, 10, None]
-formulaIncome = models.piecewiseFormula(ScaledIncome,
-                                        thresholds,
-                                        [structBetas['beta_ScaledIncome_lessthan_4'],
-                                         structBetas['beta_ScaledIncome_4_6'],
-                                         structBetas['beta_ScaledIncome_6_8'],
-                                         structBetas['beta_ScaledIncome_8_10'],
-                                         structBetas['beta_ScaledIncome_10_more']])
+formulaIncome = models.piecewiseFormula(
+    ScaledIncome,
+    thresholds,
+    [
+        structBetas['beta_ScaledIncome_lessthan_4'],
+        structBetas['beta_ScaledIncome_4_6'],
+        structBetas['beta_ScaledIncome_6_8'],
+        structBetas['beta_ScaledIncome_8_10'],
+        structBetas['beta_ScaledIncome_10_more'],
+    ],
+)
 
 # Definition of other variables
 age_65_more = DefineVariable('age_65_more', age >= 65, database)
@@ -59,60 +74,39 @@ moreThanOneCar = DefineVariable('moreThanOneCar', NbCar > 1, database)
 moreThanOneBike = DefineVariable('moreThanOneBike', NbBicy > 1, database)
 individualHouse = DefineVariable('individualHouse', HouseType == 1, database)
 male = DefineVariable('male', Gender == 1, database)
-haveChildren = DefineVariable('haveChildren', \
-((FamilSitu == 3) + (FamilSitu == 4)) > 0, database)
+haveChildren = DefineVariable(
+    'haveChildren', ((FamilSitu == 3) + (FamilSitu == 4)) > 0, database
+)
 haveGA = DefineVariable('haveGA', GenAbST == 1, database)
 highEducation = DefineVariable('highEducation', Education >= 6, database)
 
 
 ### Coefficients
 
-coef_intercept = Beta('coef_intercept',
-                      structBetas['coef_intercept'],
-                      None,
-                      None,
-                      0)
-coef_age_65_more = Beta('coef_age_65_more',
-                        structBetas['coef_age_65_more'],
-                        None,
-                        None,
-                        0)
-coef_haveGA = Beta('coef_haveGA',
-                   structBetas['coef_haveGA'],
-                   None,
-                   None,
-                   0)
+coef_intercept = Beta(
+    'coef_intercept', structBetas['coef_intercept'], None, None, 0
+)
+coef_age_65_more = Beta(
+    'coef_age_65_more', structBetas['coef_age_65_more'], None, None, 0
+)
+coef_haveGA = Beta('coef_haveGA', structBetas['coef_haveGA'], None, None, 0)
 
-coef_moreThanOneCar = Beta('coef_moreThanOneCar',
-                           structBetas['coef_moreThanOneCar'],
-                           None,
-                           None,
-                           0)
-coef_moreThanOneBike = Beta('coef_moreThanOneBike',
-                            structBetas['coef_moreThanOneBike'],
-                            None,
-                            None,
-                            0)
-coef_individualHouse = Beta('coef_individualHouse',
-                            structBetas['coef_individualHouse'],
-                            None,
-                            None,
-                            0)
-coef_male = Beta('coef_male',
-                 structBetas['coef_male'],
-                 None,
-                 None,
-                 0)
-coef_haveChildren = Beta('coef_haveChildren',
-                         structBetas['coef_haveChildren'],
-                         None,
-                         None,
-                         0)
-coef_highEducation = Beta('coef_highEducation',
-                          structBetas['coef_highEducation'],
-                          None,
-                          None,
-                          0)
+coef_moreThanOneCar = Beta(
+    'coef_moreThanOneCar', structBetas['coef_moreThanOneCar'], None, None, 0
+)
+coef_moreThanOneBike = Beta(
+    'coef_moreThanOneBike', structBetas['coef_moreThanOneBike'], None, None, 0
+)
+coef_individualHouse = Beta(
+    'coef_individualHouse', structBetas['coef_individualHouse'], None, None, 0
+)
+coef_male = Beta('coef_male', structBetas['coef_male'], None, None, 0)
+coef_haveChildren = Beta(
+    'coef_haveChildren', structBetas['coef_haveChildren'], None, None, 0
+)
+coef_highEducation = Beta(
+    'coef_highEducation', structBetas['coef_highEducation'], None, None, 0
+)
 
 ### Latent variable: structural equation
 
@@ -120,17 +114,19 @@ coef_highEducation = Beta('coef_highEducation',
 # for numerical integration
 sigma_s = Beta('sigma_s', 1, None, None, 0)
 
-CARLOVERS = coef_intercept + \
-            coef_age_65_more * age_65_more + \
-            formulaIncome + \
-            coef_moreThanOneCar * moreThanOneCar + \
-            coef_moreThanOneBike * moreThanOneBike + \
-            coef_individualHouse * individualHouse + \
-            coef_male * male + \
-            coef_haveChildren * haveChildren + \
-            coef_haveGA * haveGA + \
-            coef_highEducation * highEducation + \
-            sigma_s * bioDraws('EC', 'NORMAL_MLHS')
+CARLOVERS = (
+    coef_intercept
+    + coef_age_65_more * age_65_more
+    + formulaIncome
+    + coef_moreThanOneCar * moreThanOneCar
+    + coef_moreThanOneBike * moreThanOneBike
+    + coef_individualHouse * individualHouse
+    + coef_male * male
+    + coef_haveChildren * haveChildren
+    + coef_haveGA * haveGA
+    + coef_highEducation * highEducation
+    + sigma_s * bioDraws('EC', 'NORMAL_MLHS')
+)
 
 ### Measurement equations
 
@@ -186,7 +182,8 @@ IndEnvir01 = {
     5: 1 - bioNormalCdf(Envir01_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0}
+    -2: 1.0,
+}
 
 P_Envir01 = Elem(IndEnvir01, Envir01)
 
@@ -202,7 +199,8 @@ IndEnvir02 = {
     5: 1 - bioNormalCdf(Envir02_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0}
+    -2: 1.0,
+}
 
 P_Envir02 = Elem(IndEnvir02, Envir02)
 
@@ -218,7 +216,8 @@ IndEnvir03 = {
     5: 1 - bioNormalCdf(Envir03_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0}
+    -2: 1.0,
+}
 
 P_Envir03 = Elem(IndEnvir03, Envir03)
 
@@ -234,7 +233,7 @@ IndMobil11 = {
     5: 1 - bioNormalCdf(Mobil11_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0
+    -2: 1.0,
 }
 
 P_Mobil11 = Elem(IndMobil11, Mobil11)
@@ -251,7 +250,8 @@ IndMobil14 = {
     5: 1 - bioNormalCdf(Mobil14_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0}
+    -2: 1.0,
+}
 
 P_Mobil14 = Elem(IndMobil14, Mobil14)
 
@@ -267,7 +267,8 @@ IndMobil16 = {
     5: 1 - bioNormalCdf(Mobil16_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0}
+    -2: 1.0,
+}
 
 P_Mobil16 = Elem(IndMobil16, Mobil16)
 
@@ -283,7 +284,8 @@ IndMobil17 = {
     5: 1 - bioNormalCdf(Mobil17_tau_4),
     6: 1.0,
     -1: 1.0,
-    -2: 1.0}
+    -2: 1.0,
+}
 
 P_Mobil17 = Elem(IndMobil17, Mobil17)
 
@@ -294,8 +296,10 @@ P_Mobil17 = Elem(IndMobil17, Mobil17)
 try:
     choiceResults = res.bioResults(pickleFile='04latentChoiceSeq.pickle')
 except FileNotFoundError:
-    print('Run first the script 04latentChoiceSeq.py in order to generate the file '
-          '04latentChoiceSeq.pickle.')
+    print(
+        'Run first the script 04latentChoiceSeq.py in order to generate the '
+        'file 04latentChoiceSeq.pickle.'
+    )
     sys.exit()
 
 choiceBetas = choiceResults.getBetaValues()
@@ -303,47 +307,40 @@ choiceBetas = choiceResults.getBetaValues()
 ASC_CAR = Beta('ASC_CAR', choiceBetas['ASC_CAR'], None, None, 0)
 ASC_PT = Beta('ASC_PT', 0, None, None, 1)
 ASC_SM = Beta('ASC_SM', choiceBetas['ASC_SM'], None, None, 0)
-BETA_COST_HWH = Beta('BETA_COST_HWH', choiceBetas['BETA_COST_HWH'], None, None, 0)
-BETA_COST_OTHER = Beta('BETA_COST_OTHER',
-                       choiceBetas['BETA_COST_OTHER'],
-                       None,
-                       None,
-                       0)
+BETA_COST_HWH = Beta(
+    'BETA_COST_HWH', choiceBetas['BETA_COST_HWH'], None, None, 0
+)
+BETA_COST_OTHER = Beta(
+    'BETA_COST_OTHER', choiceBetas['BETA_COST_OTHER'], None, None, 0
+)
 BETA_DIST = Beta('BETA_DIST', choiceBetas['BETA_DIST'], None, None, 0)
-BETA_TIME_CAR_REF = Beta('BETA_TIME_CAR_REF',
-                         choiceBetas['BETA_TIME_CAR_REF'],
-                         None,
-                         0,
-                         0)
-BETA_TIME_CAR_CL = Beta('BETA_TIME_CAR_CL',
-                        choiceBetas['BETA_TIME_CAR_CL'],
-                        None,
-                        None,
-                        0)
-BETA_TIME_PT_REF = Beta('BETA_TIME_PT_REF',
-                        choiceBetas['BETA_TIME_PT_REF'],
-                        None,
-                        0,
-                        0)
-BETA_TIME_PT_CL = Beta('BETA_TIME_PT_CL',
-                       choiceBetas['BETA_TIME_PT_CL'],
-                       None,
-                       None,
-                       0)
-BETA_WAITING_TIME = Beta('BETA_WAITING_TIME',
-                         choiceBetas['BETA_WAITING_TIME'],
-                         None,
-                         None,
-                         0)
+BETA_TIME_CAR_REF = Beta(
+    'BETA_TIME_CAR_REF', choiceBetas['BETA_TIME_CAR_REF'], None, 0, 0
+)
+BETA_TIME_CAR_CL = Beta(
+    'BETA_TIME_CAR_CL', choiceBetas['BETA_TIME_CAR_CL'], None, None, 0
+)
+BETA_TIME_PT_REF = Beta(
+    'BETA_TIME_PT_REF', choiceBetas['BETA_TIME_PT_REF'], None, 0, 0
+)
+BETA_TIME_PT_CL = Beta(
+    'BETA_TIME_PT_CL', choiceBetas['BETA_TIME_PT_CL'], None, None, 0
+)
+BETA_WAITING_TIME = Beta(
+    'BETA_WAITING_TIME', choiceBetas['BETA_WAITING_TIME'], None, None, 0
+)
 
 TimePT_scaled = DefineVariable('TimePT_scaled', TimePT / 200, database)
 TimeCar_scaled = DefineVariable('TimeCar_scaled', TimeCar / 200, database)
-MarginalCostPT_scaled = DefineVariable('MarginalCostPT_scaled',
-                                       MarginalCostPT / 10, database)
-CostCarCHF_scaled = DefineVariable('CostCarCHF_scaled',
-                                   CostCarCHF / 10, database)
-distance_km_scaled = DefineVariable('distance_km_scaled',
-                                    distance_km / 5, database)
+MarginalCostPT_scaled = DefineVariable(
+    'MarginalCostPT_scaled', MarginalCostPT / 10, database
+)
+CostCarCHF_scaled = DefineVariable(
+    'CostCarCHF_scaled', CostCarCHF / 10, database
+)
+distance_km_scaled = DefineVariable(
+    'distance_km_scaled', distance_km / 5, database
+)
 PurpHWH = DefineVariable('PurpHWH', TripPurpose == 1, database)
 PurpOther = DefineVariable('PurpOther', TripPurpose != 1, database)
 
@@ -351,57 +348,61 @@ PurpOther = DefineVariable('PurpOther', TripPurpose != 1, database)
 
 BETA_TIME_PT = BETA_TIME_PT_REF * exp(BETA_TIME_PT_CL * CARLOVERS)
 
-V0 = ASC_PT + \
-     BETA_TIME_PT * TimePT_scaled + \
-     BETA_WAITING_TIME * WaitingTimePT + \
-     BETA_COST_HWH * MarginalCostPT_scaled * PurpHWH  +\
-     BETA_COST_OTHER * MarginalCostPT_scaled * PurpOther
+V0 = (
+    ASC_PT
+    + BETA_TIME_PT * TimePT_scaled
+    + BETA_WAITING_TIME * WaitingTimePT
+    + BETA_COST_HWH * MarginalCostPT_scaled * PurpHWH
+    + BETA_COST_OTHER * MarginalCostPT_scaled * PurpOther
+)
 
 BETA_TIME_CAR = BETA_TIME_CAR_REF * exp(BETA_TIME_CAR_CL * CARLOVERS)
 
-V1 = ASC_CAR + \
-      BETA_TIME_CAR * TimeCar_scaled + \
-      BETA_COST_HWH * CostCarCHF_scaled * PurpHWH  + \
-      BETA_COST_OTHER * CostCarCHF_scaled * PurpOther
+V1 = (
+    ASC_CAR
+    + BETA_TIME_CAR * TimeCar_scaled
+    + BETA_COST_HWH * CostCarCHF_scaled * PurpHWH
+    + BETA_COST_OTHER * CostCarCHF_scaled * PurpOther
+)
 
 V2 = ASC_SM + BETA_DIST * distance_km_scaled
 
 # Associate utility functions with the numbering of alternatives
-V = {0: V0,
-     1: V1,
-     2: V2}
+V = {0: V0, 1: V1, 2: V2}
 
-# Conditional to omega, we have a logit model (called the kernel) for the choice
+# Conditional to omega, we have a logit model (called the kernel) for
+# the choice
 condprob = models.logit(V, None, Choice)
 
-# Conditional to omega, we have the product of ordered probit for the indicators.
-condlike = P_Envir01 * \
-    P_Envir02 * \
-    P_Envir03 * \
-    P_Mobil11 * \
-    P_Mobil14 * \
-    P_Mobil16 * \
-    P_Mobil17 * \
-    condprob
+# Conditional to omega, we have the product of ordered probit for the
+# indicators.
+condlike = (
+    P_Envir01
+    * P_Envir02
+    * P_Envir03
+    * P_Mobil11
+    * P_Mobil14
+    * P_Mobil16
+    * P_Mobil17
+    * condprob
+)
 
 # We integrate over omega using numerical integration
 loglike = log(MonteCarlo(condlike))
 
 # Define level of verbosity
 logger = msg.bioMessage()
-#logger.setSilent()
-#logger.setWarning()
+# logger.setSilent()
+# logger.setWarning()
 logger.setGeneral()
-#logger.setDetailed()
+# logger.setDetailed()
 
 # Create the Biogeme object
 biogeme = bio.BIOGEME(database, loglike, numberOfDraws=10000)
 biogeme.modelName = '05latentChoiceFull_mc'
 
 # Estimate the parameters
-fname = '__05iterations.txt'
-biogeme.loadSavedIteration(filename=fname)
-results = biogeme.estimate(saveIterations=True, file_iterations=fname)
+results = biogeme.estimate()
 
 print(f'Estimated betas: {len(results.data.betaValues)}')
 print(f'Final log likelihood: {results.data.logLike:.3f}')
