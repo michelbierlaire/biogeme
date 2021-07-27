8ae380537e2cd2108532983b8b1069a53e5ebf13
michelbierlaire <michel.bierlaire@epfl.ch>
2021-07-27 15:56:05 +0200

    Update conf.py

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -18,11 +18,11 @@ sys.path.insert(0, os.path.abspath('.hyperlearn'))
 # -- Project information -----------------------------------------------------
 
 project = 'Biogeme'
-copyright = '2020, Michel Bierlaire'
+copyright = '2021, Michel Bierlaire'
 author = 'Michel Bierlaire'
 
 # The full version, including alpha/beta/rc tags
-release = '3.2.6'
+release = '3.2.7'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +30,11 @@ release = '3.2.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.autosectionlabel',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +53,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'setup.py']
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-
+#html_theme = 'sphinx_book_theme'
+#html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
