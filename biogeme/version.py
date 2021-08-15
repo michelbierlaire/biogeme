502700d2a9315792b1ef32c4e46d2cdcef929ffc
michelbierlaire <michel.bierlaire@epfl.ch>
2021-08-15 17:40:19 +0200

    Update version.py

--- biogeme/version.py
+++ biogeme/version.py
@@ -18,7 +18,7 @@ versionMajor = 3
 # Minor version number
 versionMinor = 2
 # Release name
-versionRelease = '8'
+versionRelease = '9a'
 author = 'Michel Bierlaire'
 urlAuthor = 'http://people.epfl.ch/michel.bierlaire'
 department = 'Transport and Mobility Laboratory'
