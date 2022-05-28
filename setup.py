0649eb0123f55d03821b0e77e3a57355eff5ba0d
michelbierlaire <michel.bierlaire@epfl.ch>
2022-05-28 15:17:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from cysetuptools import setup
 import platform
 
 if platform.system() == 'Darwin':
-    os.environ["CC"] = 'clang'
+    os.environ["CC"] = 'clang++'
     os.environ["CXX"] = 'clang++'
 else:
-    os.environ["CC"] = 'gcc'
+    os.environ["CC"] = 'g++'
     os.environ["CXX"] = 'g++'
 
 
