acd53ce75aae77840b417fa850414e4b3639f93b
Michel Bierlaire <michel.bierlaire@epfl.ch>
2023-07-14 13:41:09 +0200

    Update logging.py

--- src/biogeme/logging.py
+++ src/biogeme/logging.py
@@ -12,6 +12,11 @@ WARNING = logging.WARNING
 ERROR = logging.ERROR
 CRITICAL = logging.CRITICAL
 
+loggers = (
+    logging.getLogger('biogeme'),
+    logging.getLogger('biogeme_optimization'),
+)
+
 
 def get_screen_logger(level=WARNING):
     """Obtain a screen logger
@@ -19,23 +24,20 @@ def get_screen_logger(level=WARNING):
     :param level: level of verbosity of the logger
     :type level: int
     """
-    logger = logging.getLogger('biogeme')
-    # This has to be set to the lower level: DEBUG so that it does not
-    # supersed the handler
-    logger.setLevel(DEBUG)
-    formatter_debug = logging.Formatter(
-        '[%(levelname)s] '
-        '%(asctime)s '
-        '%(message)s '
-        '<%(filename)s:%(lineno)d>'
-    )
-    formatter_normal = logging.Formatter('%(message)s ')
-    formatter = formatter_debug if level == DEBUG else formatter_normal
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(level)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-    return logger
+    for logger in loggers:
+        # This has to be set to the lower level: DEBUG so that it does not
+        # supersed the handler
+        logger.setLevel(DEBUG)
+        formatter_debug = logging.Formatter(
+            '[%(levelname)s] ' '%(asctime)s ' '%(message)s ' '<%(filename)s:%(lineno)d>'
+        )
+        formatter_normal = logging.Formatter('%(message)s ')
+        formatter = formatter_debug if level == DEBUG else formatter_normal
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(level)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+    return loggers[0]
 
 
 def get_file_logger(filename, level=WARNING):
@@ -47,18 +49,15 @@ def get_file_logger(filename, level=WARNING):
     :param level: level of verbosity of the logger
     :type level: int
     """
-    logger = logging.getLogger('biogeme')
-    # This has to be set to the lower level: DEBUG so that it does not
-    # supersed the handler
-    logger.setLevel(DEBUG)
-    formatter = logging.Formatter(
-        '[%(levelname)s] '
-        '%(asctime)s '
-        '%(message)s '
-        '<%(filename)s:%(lineno)d>'
-    )
-    file_handler = logging.FileHandler(filename)
-    file_handler.setLevel(level)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    return logger
+    for logger in loggers:
+        # This has to be set to the lower level: DEBUG so that it does not
+        # supersed the handler
+        logger.setLevel(DEBUG)
+        formatter = logging.Formatter(
+            '[%(levelname)s] ' '%(asctime)s ' '%(message)s ' '<%(filename)s:%(lineno)d>'
+        )
+        file_handler = logging.FileHandler(filename)
+        file_handler.setLevel(level)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger[0]
