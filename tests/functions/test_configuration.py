40fed2b3488b355434c7741f5412865699214c0a
Michel Bierlaire <michel.bierlaire@epfl.ch>
2023-07-28 10:01:09 +0200

    Update test_configuration.py

--- tests/functions/test_configuration.py
+++ tests/functions/test_configuration.py
@@ -9,12 +9,13 @@ import unittest
 import biogeme.configuration as conf
 import biogeme.exceptions as excep
 
+
 class TestConfiguration(unittest.TestCase):
     def test_ctor(self):
-        s11 = conf.SelectionTuple(catalog='catalog1', selection='item1')
-        s12 = conf.SelectionTuple(catalog='catalog1', selection='item2')
-        s21 = conf.SelectionTuple(catalog='catalog2', selection='choice1')
-        s22 = conf.SelectionTuple(catalog='catalog2', selection='choice2')
+        s11 = conf.SelectionTuple(controller='catalog1', selection='item1')
+        s12 = conf.SelectionTuple(controller='catalog1', selection='item2')
+        s21 = conf.SelectionTuple(controller='catalog2', selection='choice1')
+        s22 = conf.SelectionTuple(controller='catalog2', selection='choice2')
         with self.assertRaises(excep.BiogemeError):
             _ = conf.Configuration([s11, s12])
         the_conf = conf.Configuration([s11, s22])
@@ -35,10 +36,7 @@ class TestConfiguration(unittest.TestCase):
         the_id = another_conf.get_string_id()
         self.assertEqual(the_id, the_correct_id)
 
-        the_dict = {
-            'catalog1': 'item1',
-            'catalog2': 'choice2'
-        }
+        the_dict = {'catalog1': 'item1', 'catalog2': 'choice2'}
         the_correct_id = 'catalog1:item1;catalog2:choice2'
         yet_another_conf = conf.Configuration.from_dict(the_dict)
         the_id = yet_another_conf.get_string_id()
@@ -52,17 +50,17 @@ class TestConfiguration(unittest.TestCase):
         self.assertEqual(the_id, the_correct_id)
 
     def test_set_of_catalogs(self):
-        s11 = conf.SelectionTuple(catalog='catalog1', selection='item1')
-        s21 = conf.SelectionTuple(catalog='catalog2', selection='choice1')
+        s11 = conf.SelectionTuple(controller='catalog1', selection='item1')
+        s21 = conf.SelectionTuple(controller='catalog2', selection='choice1')
         the_conf = conf.Configuration([s11, s21])
-        the_set = the_conf.set_of_catalogs()
+        the_set = the_conf.set_of_controllers()
         the_correct_set = {'catalog1', 'catalog2'}
         self.assertSetEqual(the_set, the_correct_set)
 
     def test_equal(self):
-        s11 = conf.SelectionTuple(catalog='catalog1', selection='item1')
-        s21 = conf.SelectionTuple(catalog='catalog2', selection='choice1')
-        s22 = conf.SelectionTuple(catalog='catalog2', selection='choice2')
+        s11 = conf.SelectionTuple(controller='catalog1', selection='item1')
+        s21 = conf.SelectionTuple(controller='catalog2', selection='choice1')
+        s22 = conf.SelectionTuple(controller='catalog2', selection='choice2')
         conf1 = conf.Configuration([s11, s21])
         conf2 = conf.Configuration([s11, s21])
         conf3 = conf.Configuration([s11, s22])
@@ -70,8 +68,8 @@ class TestConfiguration(unittest.TestCase):
         self.assertNotEqual(conf1, conf3)
 
     def test_get_selection(self):
-        s11 = conf.SelectionTuple(catalog='catalog1', selection='item1')
-        s21 = conf.SelectionTuple(catalog='catalog2', selection='choice1')
+        s11 = conf.SelectionTuple(controller='catalog1', selection='item1')
+        s21 = conf.SelectionTuple(controller='catalog2', selection='choice1')
         the_conf = conf.Configuration([s11, s21])
         cat1 = the_conf.get_selection('catalog1')
         self.assertEqual(cat1, 'item1')
@@ -80,7 +78,6 @@ class TestConfiguration(unittest.TestCase):
         cat3 = the_conf.get_selection('catalog3')
         self.assertIsNone(cat3)
 
-        
+
 if __name__ == '__main__':
     unittest.main()
-        
