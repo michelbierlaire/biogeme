f388f3810c0092151650c927c35c41d232386a0c
Michel Bierlaire <michel.bierlaire@epfl.ch>
2024-07-16 09:33:28 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,32 +2,25 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
-from biogeme.version import __version__
-import sphinx_gallery
+
 from sphinx_gallery.sorting import FileNameSortKey
 
-
-sys.path.insert(0, os.path.abspath(".hyperlearn"))
+from biogeme.version import __version__
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+sys.path.insert(0, os.path.abspath('./extensions'))
+
 project = 'Biogeme'
-copyright = '2023, Michel Bierlaire'
+copyright = '2024, Michel Bierlaire'
 author = 'Michel Bierlaire'
 
 # The full version, including alpha/Beta/rc tags
 release = __version__
-
+language = 'en'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -36,20 +29,28 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.doctest",
     "sphinx.ext.autosummary",
     "sphinx.ext.todo",
     "sphinx_autodoc_typehints",
     "sphinx_gallery.gen_gallery",
     "sphinx_gallery.load_style",
+    'deprecated_extension',
 ]
 
-suppress_warnings = ['autosectionlabel.*']
-
 sphinx_gallery_conf = {
+    'examples_dirs': 'examples',  # Path to your example scripts
+    'gallery_dirs': 'auto_examples',  # Path to save gallery generated output
     'within_subsection_order': FileNameSortKey,
+    'filename_pattern': '/plot_',  # Pattern to match example files
+    'remove_config_comments': True,  # Remove config comments from examples
 }
 
-templates_path = ['_templates']
+
+# Explicitly disable localization
+locale_dirs = []
+gettext_compact = False
+
 exclude_patterns = [
     "_build",
     "Thumbs.db",
@@ -64,43 +65,9 @@ autodoc_typehints = "description"
 autodoc_typehints_format = "short"
 python_use_unqualified_type_names = True
 
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
-
-
-def skip(app, what, name, obj, would_skip, options):
-    keep = [
-        "__init__",
-        "__add__",
-        "__radd__",
-        "__sub__",
-        "__rsub__",
-        "__mul__",
-        "__rmul__",
-        "__div__",
-        "__rdiv__",
-        "__truediv__",
-        "__rtruediv__",
-        "__neg__",
-        "__pow__",
-        "__rpow__",
-        "__and__",
-        "__or__",
-        "__eq__",
-        "__ne__",
-        "__le__",
-        "__ge__",
-        "__lt__",
-        "__gt__",
-    ]
-    if name in keep:
-        return False
-    return would_skip
-
-
-def setup(app):
-    app.connect("autodoc-skip-member", skip)
--- docs/source/database.rst
+++ docs/source/database.rst
@@ -1,12 +1,12 @@
-Database
-========
-The database management module.
-
 biogeme.database module
------------------------
+%%%%%%%%%%%%%%%%%%%%%%%
+Module managing the data and the interactions with Pandas.
 
 .. automodule:: biogeme.database
    :members:
    :undoc-members:
    :show-inheritance:
+
+
+   
 
