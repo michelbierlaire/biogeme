68e61c34a85fef368e87ad6f3bcc28fb59d9868d
Michel Bierlaire <michel.bierlaire@epfl.ch>
2024-08-05 18:32:19 +0200

    Update numeric_tools.py

--- src/biogeme/expressions/numeric_tools.py
+++ src/biogeme/expressions/numeric_tools.py
@@ -12,7 +12,6 @@ from biogeme.exceptions import BiogemeError
 
 logger = logging.getLogger(__name__)
 
-MAX_VALUE = np.sqrt(np.finfo(float).max)
 EPSILON = np.sqrt(np.finfo(float).eps)
 
 
@@ -23,23 +22,3 @@ def is_numeric(obj) -> bool:
 
     """
     return isinstance(obj, (int, float, bool))
-
-
-def validate(value: float, modify: bool = True) -> float:
-    """Check if the value is valid, in the sense that its absolute
-    value is lower than the square root of the maximum floating point
-
-    :param value: value to validate
-    :param modify: if True, a valid value is generated. If False, an exception is triggered.
-    """
-    if value > MAX_VALUE:
-        if modify:
-            return MAX_VALUE
-        raise BiogemeError(f'Value {value} is invalid. It cannot exceed {MAX_VALUE}')
-    if value < -MAX_VALUE:
-        if modify:
-            return -MAX_VALUE
-        raise BiogemeError(
-            f'Value {value} is invalid. It cannot be lower than {-MAX_VALUE}'
-        )
-    return value
