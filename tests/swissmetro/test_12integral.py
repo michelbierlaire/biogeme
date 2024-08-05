1e685350fbe6f21daa1fdb75c701052d20c9d546
Michel Bierlaire <michel.bierlaire@epfl.ch>
2024-08-05 18:34:00 +0200

    Update test_12integral.py

--- tests/swissmetro/test_12integral.py
+++ tests/swissmetro/test_12integral.py
@@ -76,7 +76,7 @@ class test_12integral(unittest.TestCase):
         biogeme.generate_html = False
         biogeme.generate_pickle = False
         results = biogeme.estimate()
-        self.assertAlmostEqual(results.data.logLike, -4413.770793312064, 2)
+        self.assertAlmostEqual(results.data.logLike, -4359.520506709166, 2)
 
 
 if __name__ == '__main__':
