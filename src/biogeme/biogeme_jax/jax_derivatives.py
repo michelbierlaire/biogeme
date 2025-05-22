e561ff90d627e5d68392a981adf6b427fc1dfa51
Michel Bierlaire <michel.bierlaire@epfl.ch>
2025-05-22 08:30:38 +0200

    Delete jax_derivatives.py

--- src/biogeme/biogeme_jax/jax_derivatives.py
+++
@@ -1,8 +0,0 @@
-"""Calculate the derivative of the likelihood function using Jax"""
-from biogeme.expressions import Expression
-from biogeme.function_output import BiogemeFunctionOutputSmartOutputProxy
-
-
-def calculate_derivatives(loglikelihood: Expression, gradient: bool, hessian: bool, bhhh: bool) -> (
-        BiogemeFunctionOutputSmartOutputProxy):
-
