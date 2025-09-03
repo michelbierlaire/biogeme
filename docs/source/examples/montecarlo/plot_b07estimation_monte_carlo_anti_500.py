435e8ad0b4fa4ffa18f89d61ed6718b54644415f
Michel Bierlaire <michel.bierlaire@epfl.ch>
2025-09-03 10:02:03 +0200

    Examples MonteCarlo

--- docs/source/examples/montecarlo/plot_b07estimation_monte_carlo_anti_500.py
+++ docs/source/examples/montecarlo/plot_b07estimation_monte_carlo_anti_500.py
@@ -10,12 +10,15 @@ Michel Bierlaire, EPFL
 Sun Jun 29 2025, 06:47:25
 """
 
+from b07estimation_specification import get_biogeme
 from IPython.core.display_functions import display
 
 import biogeme.biogeme_logging as blog
-from b07estimation_specification import get_biogeme
 from biogeme.expressions import Draws
-from biogeme.results_processing import get_pandas_estimated_parameters
+from biogeme.results_processing import (
+    EstimationResults,
+    get_pandas_estimated_parameters,
+)
 
 # %%
 logger = blog.get_screen_logger(level=blog.INFO)
@@ -28,9 +31,14 @@ R = 500
 the_draws = Draws('b_time_rnd', 'NORMAL_ANTI')
 the_biogeme = get_biogeme(the_draws=the_draws, number_of_draws=R)
 the_biogeme.model_name = 'b07estimation_monte_carlo_anti_500'
+results_file = f'saved_results/{the_biogeme.model_name}.yaml'
+
 
 # %%
-results = the_biogeme.estimate()
+try:
+    results = EstimationResults.from_yaml_file(filename=results_file)
+except FileNotFoundError:
+    results = the_biogeme.estimate()
 
 # %%
 print(results.short_summary())
