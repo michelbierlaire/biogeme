39940a04c1cd039c8744e380a852d92f244be7fe
Michel Bierlaire <michel.bierlaire@epfl.ch>
2025-12-25 08:34:52 +0100

    Update plot_b05_normal_mixture.py

--- docs/source/examples/bayesian_swissmetro/plot_b05_normal_mixture.py
+++ docs/source/examples/bayesian_swissmetro/plot_b05_normal_mixture.py
@@ -10,13 +10,13 @@ Michel Bierlaire, EPFL
 Thu Nov 20 2025, 11:26:01
 """
 
-from IPython.core.display_functions import display
-
 import biogeme.biogeme_logging as blog
+from IPython.core.display_functions import display
 from biogeme.bayesian_estimation import BayesianResults, get_pandas_estimated_parameters
 from biogeme.biogeme import BIOGEME
 from biogeme.expressions import Beta, DistributedParameter, Draws
 from biogeme.models import loglogit
+
 # %%
 # See the data processing script: :ref:`swissmetro_data`.
 from swissmetro_data import (
@@ -101,11 +101,11 @@ the_biogeme.model_name = 'b05_normal_mixture'
 # %%
 # Estimate the parameters.
 try:
-    results = BayesianResults.from_netcdf(
+    bayesian_results = BayesianResults.from_netcdf(
         filename=f'saved_results/{the_biogeme.model_name}.nc'
     )
 except FileNotFoundError:
-    results = the_biogeme.bayesian_estimation()
+    bayesian_results = the_biogeme.bayesian_estimation()
 
 # %%
 # Get the results in a pandas table
