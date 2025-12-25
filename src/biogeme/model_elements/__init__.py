a998ea641a40d63a49218fe090174602b4a4e231
Michel Bierlaire <michel.bierlaire@epfl.ch>
2025-12-25 18:06:42 +0100

    Update __init__.py

--- src/biogeme/model_elements/__init__.py
+++ src/biogeme/model_elements/__init__.py
@@ -1,2 +1,2 @@
-from .database_adapter import FlatPanelAdapter, RegularAdapter
+from .database_adapter import FlatPanelAdapter, ModelElementsAdapter, RegularAdapter
 from .model_elements import ModelElements
