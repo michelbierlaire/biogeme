879149d43403429acc5f3339aea87250b451eb9f
Michel Bierlaire <michel.bierlaire@epfl.ch>
2025-12-25 17:25:25 +0100

    Report on hybrid choice

--- reports/latent_book/generate_latex.py
+++ reports/latent_book/generate_latex.py
@@ -10,19 +10,21 @@ from biogeme.results_processing import (
 
 # MIMIC model
 print('=' * 20 + ' MIMIC ' + '=' * 20)
-mimic_file = 'saved_results/b01_mimic.yaml'
+mimic_file = 'saved_results/b02_mimic_ml.yaml'
 
 mimic_results = EstimationResults.from_yaml_file(filename=mimic_file)
-
+print(mimic_results.get_beta_values())
 mimic_statistics = get_latex_general_statistics(estimation_results=mimic_results)
 print(mimic_statistics)
 parameter_names = {
-    r'delta_1': '$\\delta_1$',
-    r'delta_2': r'$\delta_2$',
+    'likert_delta_0_log': r'$\ln(\delta^{\text{likert}}_0)$',
+    'likert_delta_1_log': r'$\ln(\delta^{\text{likert}}_1)$',
+    'cars_delta_0_log': r'$\ln(\delta^{\text{cars}}_0)$',
+    'cars_delta_1_log': r'$\ln(\delta^{\text{cars}}_1)$',
 }
 mimic_parameters = get_latex_estimated_parameters(
     estimation_results=mimic_results,
-    variance_covariance_type=EstimateVarianceCovariance.ROBUST,
+    variance_covariance_type=EstimateVarianceCovariance.BHHH,
     renaming_parameters=parameter_names,
 )
 
@@ -31,7 +33,7 @@ print(mimic_parameters)
 # Choice model
 print('=' * 20 + ' Choice only ' + '=' * 20)
 
-choice_file = 'saved_results/b02_choice_only.yaml'
+choice_file = 'saved_results/b01_choice_only_ml.yaml'
 
 choice_results = EstimationResults.from_yaml_file(filename=choice_file)
 
@@ -41,33 +43,14 @@ print(choice_statistics)
 
 choice_parameters = get_latex_estimated_parameters(
     estimation_results=choice_results,
-    variance_covariance_type=EstimateVarianceCovariance.ROBUST,
+    variance_covariance_type=EstimateVarianceCovariance.BHHH,
 )
 print(choice_parameters)
-
-# Choice model with latent variables: sequential estimation
-print('=' * 20 + ' Choice with latent variables: sequential ' + '=' * 20)
-
-sequential_file = 'saved_results/b03_sequential.yaml'
-
-sequential_results = EstimationResults.from_yaml_file(filename=sequential_file)
-
-sequential_statistics = get_latex_general_statistics(
-    estimation_results=sequential_results
-)
-
-print(sequential_statistics)
-
-sequential_parameters = get_latex_estimated_parameters(
-    estimation_results=sequential_results,
-    variance_covariance_type=EstimateVarianceCovariance.ROBUST,
-)
-print(sequential_parameters)
 
 # Choice model with latent variables: simultaneous estimation
 print('=' * 20 + ' Choice with latent variables: simultaneous ' + '=' * 20)
 
-simultaneous_file = 'saved_results/b03_simultaneous.yaml'
+simultaneous_file = 'saved_results/b03_hybrid_ml.yaml'
 
 simultaneous_results = EstimationResults.from_yaml_file(filename=simultaneous_file)
 
@@ -87,7 +70,7 @@ print(simultaneous_parameters)
 comparison = compare_parameters(
     {
         'Choice only': choice_results,
-        'Sequential': sequential_results,
+        'MIMIC': mimic_results,
         'Simultaneous': simultaneous_results,
     }
 )
