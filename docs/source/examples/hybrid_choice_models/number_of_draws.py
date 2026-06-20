8da80f6f8775fab87bdee09392527af309e6b15a
Michel Bierlaire <michel.bierlaire@epfl.ch>
2026-06-20 13:33:57 +0200

    Update number_of_draws.py

--- docs/source/examples/hybrid_choice_models/number_of_draws.py
+++ docs/source/examples/hybrid_choice_models/number_of_draws.py
@@ -5,7 +5,7 @@ It is intentionally isolated so that the number of draws can be modified
 in one place and propagated consistently throughout the examples.
 
 Michel Bierlaire
-Wed Jun 17 2026, 18:07:05
+Wed Jun 17 2026, 13:28:01
 """
 
 NUMBER_OF_DRAWS = 50_000
